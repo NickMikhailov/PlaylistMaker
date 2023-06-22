47573077dca435537393e7f715f43817df38c81b
nvmik <nvmikhailov@gmail.com>
2023-06-22 15:13:52 +0300

    Commit 4

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -2,10 +2,55 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+    private var searchText: String? = null
+    private lateinit var inputSearchText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        inputSearchText = findViewById<EditText>(R.id.inputSearchText)
+        val clearIcon = findViewById<ImageView>(R.id.clearIcon)
+
+    // Иконка очистки сроки поиска
+        clearIcon.setOnClickListener {
+            inputSearchText.text.clear()
+            clearIcon.visibility = View.GONE
+        }
+
+        // Show the clear icon when the text changes
+        inputSearchText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().isNotEmpty()) {
+                    clearIcon.visibility = View.VISIBLE
+                } else {
+                    clearIcon.visibility = View.GONE
+                }
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        if (savedInstanceState != null) {
+            searchText = savedInstanceState.getString("searchText")
+        }
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val searchText = inputSearchText.text.toString()
+        outState.putString("searchText", searchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchText = savedInstanceState.getString("searchText")
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -13,6 +14,37 @@ class SettingsActivity : AppCompatActivity() {
         backButton.setOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
+        }
+
+        val shareButton: ImageView = findViewById<ImageView>(R.id.share_icon)
+        shareButton.setOnClickListener {
+            val link = getString(R.string.link)
+
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, link)
+            startActivity(shareIntent)
+        }
+        val supportButton: ImageView = findViewById<ImageView>(R.id.support_icon)
+        supportButton.setOnClickListener{
+            val subject = getString(R.string.msg_to_dev_subject)
+            val message = getString(R.string.msg_to_dev_text)
+            val email = getString(R.string.email)
+
+            val supportIntent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, message)
+            }
+            if (supportIntent.resolveActivity(packageManager) != null) {
+                startActivity(supportIntent)
+            }
+        }
+        val agreementButton: ImageView = findViewById<ImageView>(R.id.agreement_icon)
+        agreementButton.setOnClickListener{
+            val url = Uri.parse(getString(R.string.uri_practicum_offer))
+            val agreementIntent = Intent(Intent.ACTION_VIEW, url)
+            startActivity(agreementIntent)
         }
     }
 }
